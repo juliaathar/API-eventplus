0d2ba9fa016a7d3730d66167d404a0158369ed21
juliaathar <juathar@gmail.com>
2023-09-18 18:21:29 -0300

    feito context

---
+++ Contexts/EventContext.cs
@@ -0,0 +1,24 @@
+﻿using apiweb.eventplus.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiweb.eventplus.Contexts
+{
+    public class EventContext : DbContext
+    {
+        public DbSet<TiposUsuario> TiposUsuario { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<TiposEvento> TiposEventos { get; set; }
+        public DbSet<Evento> Evento { get; set; }
+        public DbSet<Instituicao> Instituicao { get; set; }
+        public DbSet<PresencasEvento> PresencasEvento { get; set; }
+        public DbSet<ComentariosEvento>ComentariosEvento { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=NOTE15-S14; Database= inlock_games_codeFirst_manha; User Id= sa; Pwd= Senai@134; TrustServerCertificate= True;")
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
